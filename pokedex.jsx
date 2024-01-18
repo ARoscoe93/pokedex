7679f06f883badf3a213a3dfaecdf1bd1fc51deb
ARoscoe93 <andy_roscoe12@hotmail.co.uk>
2024-01-18 13:09:41 +0000

    not working currently

--- pokedex.jsx
+++ pokedex.jsx
@@ -2,12 +2,18 @@ import React, { useState, useEffect } from "react";
 
 const Pokedex = (props) => {
   const [pokemonList, setPokemonList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const fetchPokemons = async () => {
     try {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
       const data = await response.json();
       setPokemonList(data.results);
+      setLoading(false);
     } catch (error) {
       console.error("Error fetching Pokemon data:", error);
     }
@@ -18,22 +24,26 @@ const Pokedex = (props) => {
   }, []);
 
   return (
-  <div className="pokedex">
-    <h2>All Pokemon</h2>
-    <ul>
-      {pokemonList.map(pokemon => {
-        const parts = pokemon.url.split('/');
-        const pokemonId = parts[parts.length - 2];
-        return (
-          <li key={pokemonId}>
-            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`} alt={pokemon.name} />
-            {pokemon.name}
-          </li>
-        );
-      })}
-    </ul>
-  </div>
-);
+    <div className="pokedex">
+      <h2>All Pokemon</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {pokemonList.map((pokemon, index) => {
+            const parts = pokemon.url.split('/');
+            const pokemonId = parts[parts.length - 2];
+            return (
+              <li key={index}>
+                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`} alt={pokemon.name} />
+                {pokemon.name}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Pokedex;
